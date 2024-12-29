0d6aca6363c32961e111a04a7744c2847d692a8a
Wallacy V. <wlcsv8080@outlook.com>
2024-12-28 21:03:53 -0300

    Será que eu consigo terminar isso pelo PC?

---
+++ cards/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+# from django.views.generic import TemplateView
+
+from . import views
+
+urlpatterns = [
+    path(
+        "",
+        views.CardListView.as_view(),
+        name="card-list"
+    ),
+    path(
+        "new",
+        views.CardCreateView.as_view(),
+        name="card-create"
+    ),
+    path(
+        "edit/<int:pk>",
+        views.CardUpdateView.as_view(),
+        name="card-update"
+    ),
+]
